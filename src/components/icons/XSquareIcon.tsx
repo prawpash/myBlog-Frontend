eb0e1b7afd2e060b551d16921d27ee52b742d73b
prawira <robbyprawira3@gmail.com>
2023-09-21 22:00:28 +0700

    🍱 Add New Icon Component

---
+++ src/components/icons/XSquareIcon.tsx
@@ -0,0 +1,5 @@
+export default function XSquareIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-square"><rect width="18" height="18" x="3" y="3" rx="2" ry="2" /><path d="m15 9-6 6" /><path d="m9 9 6 6" /></svg>
+  )
+}
