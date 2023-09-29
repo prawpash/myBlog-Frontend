f5bb93dd2367aa061783d1cf2d9a42057a79b7d8
prawira <robbyprawira3@gmail.com>
2023-09-30 05:36:22 +0700

    👔 Updating Route
    Updating route to use static data

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,37 @@
 import Root from "@/pages/Root"
 import ErrorPage from "@/pages/errors/error"
 import App from "./App"
+import { RouteObject } from "react-router-dom"
+import Post from "./pages/Post"
+import { loader as PostLoader } from "@/pages/Post"
+import PostCreate from "./pages/PostCreate"
 
-const routes = [
+export async function rootLoader() {
+  const data = [
+    {
+      date: "08-08-2023", title: "Judul 1"
+    },
+    {
+      date: "08-08-2023", title: "Judul 2"
+    },
+    {
+      date: "08-08-2023", title: "Judul 3"
+    },
+    {
+      date: "08-08-2023", title: "Judul 4"
+    },
+    {
+      date: "08-08-2023", title: "Judul 5"
+    },
+    {
+      date: "08-08-2023", title: "Judul 6"
+    },
+  ]
+
+  return data
+}
+
+const routes: RouteObject[] = [
   {
     path: '/',
     element: <Root />,
@@ -10,11 +39,22 @@ const routes = [
     children: [
       {
         path: '/',
+        loader: rootLoader,
         element: <App />
       },
       {
         path: '/home',
+        loader: rootLoader,
         element: <App />
+      },
+      {
+        path: '/post/:slug',
+        loader: PostLoader,
+        element: <Post />
+      },
+      {
+        path: '/admin/post/create',
+        element: <PostCreate />
       }
     ]
   }
