b03bd33fc09a4179f0c263b72cff6bd41c5f5d41
prawira <robbyprawira3@gmail.com>
2023-09-30 06:30:56 +0700

    👔 Update Root Page
    Update root page to show navigation more dinamyc

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,56 @@
+import searchIcon from "@/assets/search.svg"
 import Navbar from "@/components/Navbar";
-import { Outlet } from "react-router-dom";
+import PlusSquareIcon from "@/components/icons/PlusSquareIcon";
+import { Link, Outlet } from "react-router-dom";
+
+const navigations = [
+  {
+    name: "Home",
+    destination: "/home"
+  },
+  {
+    name: "Tags",
+    destination: "/tags"
+  },
+  {
+    name: "About",
+    destination: "/about"
+  },
+
+]
+
+function CreateButton() {
+  return (
+    <Link to={'/admin/post/create'} className="group flex justify-center items-center gap-3 hover:bg-black hover:text-white cursor-pointer">
+      <div>
+        CREATE POST
+      </div>
+
+      <PlusSquareIcon className="group-hover:stroke-white" />
+    </Link>
+  )
+}
+
+function SearchInput() {
+  return (
+    <>
+      <label htmlFor="searchInput">
+        <img src={searchIcon} alt="search icon" className="w-5 h-5" />
+      </label>
+      <input id="searchInput" type="text" placeholder="Search" className="w-full outline-none" />
+    </>
+  )
+}
 
 export default function Root() {
   return (
     <>
-      <Navbar />
-      <main className="max-w-[1080px] mt-52 mx-auto grid grid-cols-4 gap-y-4 place-items-center">
+      <Navbar
+        prefix={<CreateButton />}
+        navigations={navigations}
+        suffix={<SearchInput />}
+      />
+      <main className="max-w-[1080px] mt-52 mx-auto px-1.5">
         <Outlet />
       </main>
     </>
